eb2c27e1d3796efec445b5f926d34da3f537d2e8
alvaro salinero cepas <Alvaro@iMac-de-alvaro.local>
2016-11-02 20:00:43 +0100

    code review update

--- app/api/loms/index.js
+++ app/api/loms/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var express = require('express');
-var controller = require('./lom.controller.js');
+var controller = require('./loms.controller.js');
 
 var router = express.Router();
 
