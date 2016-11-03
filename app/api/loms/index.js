9f993479fabad20739b6e4a735dfdbe82eec07fe
fpromero <fpromero@gmail.com>
2016-11-03 01:16:03 +0100

    Change routes

--- app/api/loms/index.js
+++ app/api/loms/index.js
@@ -5,8 +5,26 @@ var controller = require('./loms.controller.js');
 
 var router = express.Router();
 
+
+
+
+// GETs
+router.get('/', controller.all);
+router.get('/:id', controller.get);
+
+// POSTS
+router.post('/', controller.create);
+
+
 // PUTs
-router.put('/me', controller.create);
+router.put('/:id', controller.update);
+
+// DELETEs
+router.delete('/',  controller.destroy);
+router.delete('/:id',  controller.remove);
+
+
+
 
 
 module.exports = router;
