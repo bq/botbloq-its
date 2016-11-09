47681177c0bb18bd3c679736b4abdc2d9a6eb082
Andres Soto Villaverde <soto_andres@yahoo.com>
2016-11-09 15:21:27 +0100

    Add files via upload
    se le agregó el campo 'code' al schema de los cursos, como un identificador único, no repetible de los mismos

---
+++ courses.model.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// var mongoose = require('mongoose');
+var mongoose = require('mongoose')
+  , Schema = mongoose.Schema;
+  					
+var Lo = new Schema({
+	name: String,
+	lo : String	
+})
+
+var Lesson = new Schema({
+	name: String,
+    resume  : String,
+	los : [Lo]	
+})
+
+var Section = new Schema({
+	name: String,
+    resume  : String, 
+	lessons  : [Lesson]
+});
+
+var CoursesSchema = new mongoose.Schema(
+	{
+	"name": { type: String, trim: true, required: true },
+	"code": { type: String, index: { unique: true } },
+	"content": { type: String, trim: true },
+	"objetives": { type: String, trim: true },
+	"bibliography": { type: String, trim: true },
+	// "sections": {type: object, trim: true }
+	sections  : [Section]
+	}, 
+	{ timestamps: true }
+	);
+	
+module.exports = mongoose.model('Courses', CoursesSchema);
