e3c4d5605130f065b76c69a7799949ff14e3c05d
fpromero <fpromero@gmail.com>
2016-11-14 19:45:16 +0100

    Recover Master version of this file

--- index.js
+++ index.js
@@ -1,41 +1,7 @@
 'use strict';
 
-var express = require('express');
-var controller = require('./courses.controller.js');
-var section_ctrl = require('./sections.controller.js');
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var router = express.Router();
-
-/*
-**************SECTIONS******************************
-*/
-
-// GETs
-router.get('/course/:course_id', section_ctrl.all_sections); // list all sections from course
-
-// POSTs
-router.post('/create_section', section_ctrl.create_section);
-// router.post('/term/:term/location/:location', section_ctrl.prueba); // create a Section by the object
-// router.get('/term/:term/location/:location', controller.prueba);
-// router.post('/term/:courses/location/:sections', controller.create);
-
-/*
-**************COURSES******************************
-*/
-
-// GETs
-router.get('/', controller.all); // list all courses
-router.get('/:id', controller.get); // list the indicated course 
-
-// POSTs
-router.post('/', controller.create); // create a Course by the object
-
-// PUTs
-router.put('/update_field', controller.update_field); // update just one field of the course
-router.put('/:id', controller.update); // update the whole course 
-
-// DELETEs
-router.delete('/:id', controller.remove);
-
-
-module.exports = router;
+// Export the application
+exports = module.exports = require('./app/app');
