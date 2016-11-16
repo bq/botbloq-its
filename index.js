ccd76b12fafe4a4a46766c2c799e148d8c655618
fpromero <fpromero@gmail.com>
2016-11-16 17:44:20 +0100

    Restore Master Version of the file

--- index.js
+++ index.js
@@ -1,52 +1,7 @@
 'use strict';
 
-var express = require('express');
-var controller = require('./courses.controller.js');
-var section_ctrl = require('./sections.controller.js');
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-var router = express.Router();
-
-/*
-**************SECTIONS******************************
-*/
-
-// GETs
-router.get('/course/:course_id', section_ctrl.all_sections); // list all sections from course
-router.get('/course/:course_id/section/:section_id', section_ctrl.get_section); // list the indicated section
-
-// DELETEs
-router.delete('/course/:course_id/section/:section_id', section_ctrl.delete_section);
-
-// POSTs
-router.put('/create_section', section_ctrl.create_section);
-// router.put('/create_section2', section_ctrl.create_section2);
-// router.post('/term/:term/location/:location', section_ctrl.prueba); // create a Section by the object
-// router.get('/term/:term/location/:location', controller.prueba);
-// router.post('/term/:courses/location/:sections', controller.create);
-
-// PUTs
-router.put('/update_section_field', section_ctrl.update_section_field); // update a particular field 
-	// of a section of a course
-router.put('/update_section', section_ctrl.update_section); // update a particular section of a course
-
-
-/*
-***************COURSES******************************
-*/
-
-// GETs
-router.get('/', controller.all); // list all courses
-router.get('/:id', controller.get); // list the indicated course 
-
-// POSTs
-router.post('/', controller.create); // create a Course by the object
-
-// PUTs
-router.put('/update_field', controller.update_field); // update just one field of the course
-router.put('/:id', controller.update); // update the whole course 
-
-// DELETEs
-router.delete('/:id', controller.remove);
-
-
-module.exports = router;
+// Export the application
+exports = module.exports = require('./app/app');
