ca44adcb32e4fb548af4961d0de735aa43a3bc9f
Andres Soto Villaverde <soto_andres@yahoo.com>
2016-11-24 09:23:03 +0100

    Learning objectives according to taxonomy of Bloom
    Learning objectives can be associated with Courses, Sections or Lessons.
    Each one of them must be an object that indicates its relation with the taxonomy of Bloom: to which domain belongs and the concrete learning objective according to that taxonomy. In addition, a field is included for the specific description of the objective.

--- app/api/courses/courses.model.js
+++ app/api/courses/courses.model.js
@@ -3,26 +3,52 @@
 // var mongoose = require('mongoose');
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
-  										
-var Lo = new Schema( // { autoIndex: false },
+      										
+var LOM = new Schema( // { autoIndex: false },
 	{ lom_id: String } 
 	);
+	
+var Objective = new Schema(
+	{
+		description: String,
+		bloom_domain: {
+			type:String,
+			enum:["cognitive", "affective", "psychomotor"]
+			},		
+		domain: {
+			type:String,
+			enum:[
+			"Applying", "Analyzing", "Synthesizing", "Evaluating",
+			"Receiving", "Responding", "Valuing", "Organizing", "Characterizing",
+			"Perception", "Set", "Guided response", "Mechanism", "Complex overt response", 
+					"Adaptation", "Origination"
+			]
+			}
+	});
 
 var Lesson = new Schema( // { autoIndex: false },
-	{ name: String, resume  : String, los : [Lo] }
+	{ 
+	name: String, 
+	resume  : String,  
+	objetives: [Objective],
+	loms : [LOM] }
 	);
 
 var Section = new Schema( // { autoIndex: false },
-	{ name: String, resume  : String, lessons  : [Lesson]}
+	{ 
+	name: String, 
+	resume  : String, 
+	objetives: [Objective],
+	lessons  : [Lesson]}
 	);
-
+	
 var CoursesSchema = new mongoose.Schema(
 	{
-	"name": { type: String, trim: true, required: true },
-	"code": { type: String, index: { unique: true } },
-	"content": { type: String, trim: true },
-	"objetives": { type: String, trim: true },
-	"bibliography": { type: String, trim: true },
+	name: { type: String, trim: true, required: true },
+	code: { type: String, index: { unique: true } },
+	content: { type: String, trim: true },
+	objetives: [Objective],
+	bibliography: { type: String, trim: true },
 	sections  : [Section]
 	}, 
 	{ timestamps: true }
