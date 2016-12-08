5f25bd9f15f308864fa3ee0d711f70f37ef60df5
Álvaro Salinero Cepas <salinerocepas.alvaro@gmail.com>
2016-12-08 13:57:08 +0100

    included learning_path

--- app/api/courses/courses.model.js
+++ app/api/courses/courses.model.js
@@ -1,50 +1,50 @@
 'use strict';
-
-// var mongoose = require('mongoose');
 var mongoose = require('mongoose')
   , Schema = mongoose.Schema;
       										
-var LOM = new Schema( // { autoIndex: false },
-	{ lom_id: String,
-	_id: false } 
-	);
+var LOM = new Schema({ 
+	lom_id: String,
+	_id: false 
+});
 	
-var Objective = new Schema(
-	{
-		description: String,
-		bloom_domain: {
-			type:String,
-			enum:["cognitive", "affective", "psychomotor"]
-			},		
-		domain: {
-			type:String,
-			enum:[
-			"Applying", "Analyzing", "Synthesizing", "Evaluating",
-			"Receiving", "Responding", "Valuing", "Organizing", "Characterizing",
-			"Perception", "Set", "Guided response", "Mechanism", "Complex overt response", 
-					"Adaptation", "Origination"
-			]
-			}
-	});
+var Objective = new Schema({
+	description: String,
+	bloom_domain: {
+		type:String,
+		enum:["cognitive", "affective", "psychomotor"]
+	},		
+	domain: {
+		type:String,
+		enum:[
+		"Applying", "Analyzing", "Synthesizing", "Evaluating",
+		"Receiving", "Responding", "Valuing", "Organizing", "Characterizing",
+		"Perception", "Set", "Guided response", "Mechanism", "Complex overt response", 
+				"Adaptation", "Origination"
+		]
+	}
+});
 
-var Lesson = new Schema( // { autoIndex: false },
-	{ 
-	name: String, 
-	resume  : String,  
-	objectives: [Objective],
-	loms : [LOM] }
-	);
+var Lesson = new Schema({ 
+		name: String, 
+		resume: String,  
+		objectives: [Objective],
+		learning_path: {
+			ok: [Number],
+			nok: [Number]
+		},
+		loms : [LOM],
+		_id: false
+});
 
-var Section = new Schema( // { autoIndex: false },
-	{ 
+var Section = new Schema({ 
 	name: String, 
 	resume  : String, 
 	objectives: [Objective],
-	lessons  : [Lesson]}
-	);
+	lessons  : [Lesson],
+	_id: false
+});
 	
-var CoursesSchema = new mongoose.Schema(
-	{
+var CoursesSchema = new mongoose.Schema({
 	name: { type: String, trim: true, required: true },
 	code: { type: String, index: { unique: true } },
 	content: { type: String, trim: true },
